87c7503ac23a7dda77825456fb806ad9ef460c25
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2020-04-28 18:55:00 -0300

    Configuration eslint

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-const routes = Router();
-
-routes.get('/', (request, response) =>
-  response.json({message: 'Hello Word DEV!!!'}));
-
-export default routes;
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import routes from './routes';
 
 const app = express();
 
+app.routes(routes);
+
 app.get('/', (request, response) => response.json({ message: 'Hello Word DEV!' }));
 
-// eslint-disable-next-line no-console
 app.listen(3333, () => console.log('✅ Sevidor Rodando na porta 3333'));
