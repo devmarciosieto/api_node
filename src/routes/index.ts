590dd1879e5abac25c6e4ea7f01e906085703514
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2020-04-28 20:20:56 -0300

    bk

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello Word DEV!!!' }));
+routes.get('/', (request, response) =>
+response.json({ message: 'Hello Word DEV!!!' }));
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-
 import routes from './routes';
 
 const app = express();
 
-app.routes(routes);
-
-app.get('/', (request, response) => response.json({ message: 'Hello Word DEV!' }));
+app.use(routes);
 
 app.listen(3333, () => console.log('✅ Sevidor Rodando na porta 3333'));
