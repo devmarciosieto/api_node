93b2c5a8f923d173fd9392d127486cd5340fc87b
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2020-04-30 02:53:20 -0300

    começando a Models de Agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,26 +1,34 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
-const appintments = [];
+import Appointment from '../models/Appointment';
 
 const appintmentsRouter = Router();
 
+const appintments: Appointment[] = [];
+
 appintmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  console.log(request.body)
-  /*
-  console.log(request.body)
 
-  const appintment = {
-    id: uuid(),
-    provider,
-    date,
+  const parseDate = startOfHour(parseISO(date));
+
+
+
+const horaDisponivel = appintments.find(vairavel =>
+    isEqual(parseDate, vairavel.date),
+    );
+
+
+
+  const appintment = new Appointment(provider, parseDate)
+
+  if (horaDisponivel){
+    return response.status(400).json({ message: 'Este horario já está marcado!'})
   }
 
   appintments.push(appintment);
 
-  return response.json(appintment);*/
-  return response.json({m:"sdfs"})
+  return response.json(appintment);
 });
 
 export default appintmentsRouter;
